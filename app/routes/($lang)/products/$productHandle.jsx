ca343a167cc2188aad42c94a3e80b896b1c87989
wjsacken <wsackenheim@artscube.biz>
2023-05-05 08:25:11 -0400

    Update $productHandle.jsx

--- app/routes/($lang)/products/$productHandle.jsx
+++ app/routes/($lang)/products/$productHandle.jsx
@@ -1,7 +1,7 @@
 import {useMemo, useTransition} from 'react';
 import {Disclosure} from '@headlessui/react';
 import {defer} from '@shopify/remix-oxygen';
-import {useLoaderData, Await, useSearchParams} from '@remix-run/react';
+import {useLoaderData, useSearchParams} from '@remix-run/react';
 import {AnalyticsPageType, Money} from '@shopify/hydrogen';
 import {
   Heading,
@@ -141,6 +141,13 @@ export default function Product() {
           </div>
         </div>
       </Section>
+      {(products) => (
+        <ProductSwimlane
+          title="Related Products"
+          products={products.slice(0, 6)}
+          count="6"
+        />
+      )}
     </>
   );
 }
