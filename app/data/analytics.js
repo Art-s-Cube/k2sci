15e375cc30f3003d0741672a896950ab773ad580
wjsacken <wsackenheim@artscube.biz>
2023-05-16 14:04:43 -0400

    Update analytics.js

--- app/data/analytics.js
+++ app/data/analytics.js
@@ -1,1 +1,17 @@
 
+import React from 'react';
+
+class analyticsEmbed extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-KKJFP4WYF2';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default analyticsEmbed;
