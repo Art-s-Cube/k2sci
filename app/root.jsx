49dcaac2aca1002f8ed2c4391bac2d7538a5417e
wjsacken <wsackenheim@artscube.biz>
2023-05-17 08:39:06 -0400

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -80,11 +80,12 @@ export default function App() {
         <Seo />
         <Links />
         <Helmet>
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script>
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=Y-iS3sG6bb2Cphm8C4Evzw&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-P59LZWJ');
+            })(window,document,'script','dataLayer','GTM-P59LZWJ');`}
           </script>
         </Helmet>
       </head>
