9dd4e0295fb67d28324b035c4d274f59fce5aaf7
wjsacken <wsackenheim@artscube.biz>
2023-05-17 09:50:10 -0400

    Update root.jsx6

--- app/root.jsx
+++ app/root.jsx
@@ -91,10 +91,8 @@ export default function App() {
       </head>
       <body>
         <Helmet>
-          <noscript>
-            {`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P59LZWJ&gtm_auth=Y-iS3sG6bb2Cphm8C4Evzw&gtm_preview=env-1&gtm_cookies_win=x"
-              height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>`}
-          </noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P59LZWJ&gtm_auth=Y-iS3sG6bb2Cphm8C4Evzw&gtm_preview=env-1&gtm_cookies_win=x"
+              height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
         </Helmet>
         <Layout
           layout={data.layout}
