91669dee4decfd9ebbc0150924411b0902453ef7
wjsacken <wsackenheim@artscube.biz>
2023-05-17 08:33:29 -0400

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -80,22 +80,12 @@ export default function App() {
         <Seo />
         <Links />
         <Helmet>
-          <script
-            async
-            src='https://www.googletagmanager.com/gtag/js?id=G-KKJFP4WYF2'
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KKJFP4WYF2', {
-            page_path: window.location.pathname,
-          });
-          `,
-            }}
-          />
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=Y-iS3sG6bb2Cphm8C4Evzw&gtm_preview=env-1&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P59LZWJ');
+          </script>
         </Helmet>
       </head>
       <body>
